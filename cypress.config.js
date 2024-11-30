65a4866ee80b3697298bcb6925d3fdc3a0fde2fe
Everton Araujo <everton.oa@gmail.com>
2024-11-30 10:43:41 +0000

    :bug: Fix screen config

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportHeight: 1920,
-  viewportWidth: 1080,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   video: false, 
   e2e: {
     baseUrl: "http://localhost:4200",
