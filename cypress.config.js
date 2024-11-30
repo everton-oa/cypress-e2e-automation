b45e176613263a04c3d34d6ea77368f6e562cbe8
Everton Araujo <everton.oa@gmail.com>
2024-11-30 09:42:47 +0000

    :wrench: Change cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1920,
+  viewportWidth: 1080,
+  video: false, 
   e2e: {
+    baseUrl: "http://localhost:4200",
+    excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
