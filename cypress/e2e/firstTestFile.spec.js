8baa293f7ec90341d6b7cec73ad4ef9fa3c8d1d1
Everton Araujo <everton.oa@gmail.com>
2024-12-03 06:23:28 +0000

    :construction: Class30-PopUps and Tooltips

--- cypress/e2e/firstTestFile.spec.js
+++ cypress/e2e/firstTestFile.spec.js
@@ -273,7 +273,7 @@ describe("First test suite", () => {
     });
   });
 
-  it.only("Tenth test - Web Tables 1", () => {
+  it("Tenth test - Web Tables 1", () => {
     cy.visit("/");
     cy.contains("Tables & Data").click();
     cy.contains("Smart Table").click();
@@ -319,5 +319,43 @@ describe("First test suite", () => {
         }
       });
     });
+  });
+
+  it("Eleventh test - Tooltip", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Tooltip").click();
+
+    cy.contains("nb-card", "Colored Tooltips").contains("Default").click();
+    cy.get("nb-tooltip").should("contain", "This is a tooltip");
+  });
+
+  it.only("Twelfth test - Dialog box", () => {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    // 1
+    // cy.get("tbody tr").first().find(".nb-trash").click();
+    // cy.on("window:confirm", (confirm) => {
+    //   expect(confirm).to.equal("Are you sure you want to delete?");
+    // });
+
+    // 2
+    // const stub = cy.stub();
+    // cy.on("window:confirm", stub);
+    // cy.get("tbody tr")
+    //   .first()
+    //   .find(".nb-trash")
+    //   .click()
+    //   .then(() => {
+    //     expect(stub.getCall(0)).to.be.calledWith(
+    //       "Are you sure you want to delete?"
+    //     );
+    //   });
+
+    // 3
+    cy.get("tbody tr").first().find(".nb-trash").click();
+    cy.on("window:confirm", () => false);
   });
 });
