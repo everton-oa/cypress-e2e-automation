4929ecb76ce0886eccc61d3eac5a345b2382efd8
Everton Araujo <64025923+everton-oa@users.noreply.github.com>
2024-12-04 05:10:17 +0000

    :construction: Class33-E2E flow with Page Object (#7)

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
+import { onDatepickerPage } from "../support/page-objects/datepickerPage";
+import { onFormLayoutPage } from "../support/page-objects/formLayoutPage";
 import { navigateTo } from "../support/page-objects/navigationPage";
+import { onSmartTablePage } from "../support/page-objects/smartTablePage";
 
 describe("First test suite", () => {
   beforeEach("Open application", () => {
@@ -13,5 +16,25 @@ describe("First test suite", () => {
     navigateTo.smartTablePage();
     navigateTo.tooltipPage();
     navigateTo.toasterPage();
+  });
+
+  it.only("Should submmit inline and basic form and select tomorrow date in the calendar", () => {
+    navigateTo.formLayoutsPage();
+    onFormLayoutPage.submitInLineFormWithNameAndEmail(
+      "Everton",
+      "test@test.com"
+    );
+    onFormLayoutPage.submitBasicFormWithEmailAndPassword(
+      "test@test.com",
+      "pass"
+    );
+    navigateTo.formDatepickerPage();
+    onDatepickerPage.selectCommonDatepickerDateFromToday(1);
+    onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14);
+
+    navigateTo.smartTablePage();
+    onSmartTablePage.addNewRecordWithFirstAndLastName("John", "Doe");
+    onSmartTablePage.updateAgeByFirstName("John", "35");
+    onSmartTablePage.deleteRowByIndex(1);
   });
 });
