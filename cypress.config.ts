b07585babb76c002a67840177bce7622eae5f6ec
Everton Araujo <everton.oa@gmail.com>
2024-12-04 18:23:49 +0000

    :wrench: Add env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,11 +6,7 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
-  env: {
-    username: "everton.araujo@test.com",
-    password: "Cypress123",
-    apiUrl: "https://api.realworld.io",
-  },
+
   e2e: {
     baseUrl: "http://localhost:4200",
     excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
@@ -19,7 +15,10 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     env: {
-      conduitBaseUrl: "https://conduit.bondaracademy.com/",
+      username: "everton.araujo@test.com",
+      password: "Cypress123",
+      conduitBaseUrl: "https://conduit.bondaracademy.com",
+      conduitApiBaseUrl: "https://conduit-api.bondaracademy.com",
       specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     },
   },
